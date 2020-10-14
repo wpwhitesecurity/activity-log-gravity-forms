c12cad864fcbaf22fbdba6fad20d5ca9a64e0cd6
Danny Jones <daniel@wpwhitesecurity.com>
2020-10-14 18:15:01 +0100

    Correcting object label
    Fixes #29

--- includes/wsal-functions.php
+++ includes/wsal-functions.php
@@ -63,7 +63,7 @@ function wsal_gravityforms_add_custom_event_objects( $objects ) {
 		'gravityforms_forms'         => esc_html__( 'Forms in Gravity Forms', 'wsal-gravity-forms' ),
 		'gravityforms_confirmations' => esc_html__( 'Confirmations in Gravity Forms', 'wsal-gravity-forms' ),
 		'gravityforms_notifications' => esc_html__( 'Notifications in Gravity Forms', 'wsal-gravity-forms' ),
-		'gravityforms_entries'       => esc_html__( 'Notifications in Gravity Forms', 'wsal-gravity-forms' ),
+		'gravityforms_entries'       => esc_html__( 'Entries in Gravity Forms', 'wsal-gravity-forms' ),
 		'gravityforms_fields'        => esc_html__( 'Fields in Gravity Forms', 'wsal-gravity-forms' ),
 		'gravityforms_settings'      => esc_html__( 'Settings in Gravity Forms', 'wsal-gravity-forms' ),
 	);
