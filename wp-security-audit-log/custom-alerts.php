8858acdf17480ec3392ba28ba623b2a70468d36a
Stoil Dobrev <sdobreff@gmail.com>
2021-03-30 08:48:04 +0300

    Changed alerts to use new WSAL format

--- wp-security-audit-log/custom-alerts.php
+++ wp-security-audit-log/custom-alerts.php
@@ -1,174 +1,303 @@
 <?php
 
-$custom_alerts = array(
-	__( 'Gravity Forms', 'wsal-gravity-forms' ) => array(
-		__( 'Monitor Gravity Forms', 'wsal-gravity-forms' ) => array(
+$custom_alerts = [
+    __( 'Gravity Forms', 'wsal-gravity-forms' ) => [
+        __( 'Monitor Gravity Forms', 'wsal-gravity-forms' ) => [
 
-			array(
-				5700,
-				WSAL_LOW,
-				__( 'A form was created, modified', 'wsal-gravity-forms' ),
-				__( 'Form name: %form_name% %LineBreak% Form ID: %form_id% %LineBreak% %EditorLinkForm%', 'wsal-gravity-forms' ),
-				'gravityforms_forms',
-				'created',
-			),
+            [
+                5700,
+                WSAL_LOW,
+                __( 'A form was created, modified', 'wsal-gravity-forms' ),
+                __( 'A form was created, modified', 'wsal-gravity-forms' ),
 
-			array(
-				5701,
-				WSAL_MEDIUM,
-				__( 'A form was moved to trash', 'wsal-gravity-forms' ),
-				__( 'Moved the form to trash %LineBreak% Form name: %form_name% %LineBreak% Form ID: %form_id% %LineBreak% %EditorLinkForm%', 'wsal-gravity-forms' ),
-				'gravityforms_forms',
-				'created',
-			),
+                [
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EditorLinkForm%',
+                ],
+                'gravityforms_forms',
+                'created',
+            ],
 
-			array(
-				5702,
-				WSAL_MEDIUM,
-				__( 'A form was permanently deleted', 'wsal-gravity-forms' ),
-				__( 'Permanently deleted the form %form_name% %LineBreak% Form ID: %form_id%', 'wsal-gravity-forms' ),
-				'gravityforms_forms',
-				'created',
-			),
+            [
+                5701,
+                WSAL_MEDIUM,
+                __( 'A form was moved to trash', 'wsal-gravity-forms' ),
+                __( 'Moved the form to trash', 'wsal-gravity-forms' ),
+                [
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EditorLinkForm%',
+                ],
+                'gravityforms_forms',
+                'created',
+            ],
 
-			array(
-				5703,
-				WSAL_MEDIUM,
-				__( 'A form setting was modified', 'wsal-gravity-forms' ),
-				__( 'The setting %setting_name% in form %form_name% %LineBreak% Previous value: %old_setting_value% %LineBreak% New value: %setting_value% %LineBreak% Form ID: %form_id% %LineBreak% %EditorLinkForm%', 'wsal-gravity-forms' ),
-				'gravityforms_forms',
-				'modified',
-			),
+            [
+                5702,
+                WSAL_MEDIUM,
+                __( 'A form was permanently deleted', 'wsal-gravity-forms' ),
+                __( 'A form was permanently deleted', 'wsal-gravity-forms' ),
+                [
+                    __( 'Permanently deleted the form ', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [],
+                'gravityforms_forms',
+                'created',
+            ],
 
-			array(
-				5704,
-				WSAL_LOW,
-				__( 'A form was duplicated', 'wsal-gravity-forms' ),
-				__( 'Source form: %original_form_name% %LineBreak% New form name: %new_form_name% %LineBreak% Source form ID: %original_form_id% %LineBreak% New form ID: %new_form_id% %LineBreak% %EditorLinkFormDuplicated%', 'wsal-gravity-forms' ),
-				'gravityforms_forms',
-				'duplicated',
-			),
+            [
+                5703,
+                WSAL_MEDIUM,
+                __( 'A form setting was modified', 'wsal-gravity-forms' ),
+                __( 'The setting %1$setting_name% in form %2$form_name%', 'wsal-gravity-forms' ),
+                [
+                    __( 'Previous value', 'wsal-gravity-forms' ) => '%old_setting_value%',
+                    __( 'New value', 'wsal-gravity-forms' ) => '%setting_value%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EditorLinkForm%',
+                ],
+                'gravityforms_forms',
+                'modified',
+            ],
 
-			array(
-				5715,
-				WSAL_MEDIUM,
-				__( 'A field was created, modified or deleted', 'wsal-gravity-forms' ),
-				__( 'Field name: %field_name% %LineBreak% Field type: %field_type% %LineBreak% Form name: %form_name% %LineBreak% Form ID: %form_id% %LineBreak% %EditorLinkForm%', 'wsal-gravity-forms' ),
-				'gravityforms_fields',
-				'created',
-			),
+            [
+                5704,
+                WSAL_LOW,
+                __( 'A form was duplicated', 'wsal-gravity-forms' ),
+                __( 'A form was duplicated', 'wsal-gravity-forms' ),
+                [
+                    __( 'Source form', 'wsal-gravity-forms' ) => '%original_form_name%',
+                    __( 'New form name', 'wsal-gravity-forms' ) => '%new_form_name%',
+                    __( 'Source form ID', 'wsal-gravity-forms' ) => '%original_form_id%',
+                    __( 'New form ID', 'wsal-gravity-forms' ) => '%new_form_id%',
+                ],
+                [
+                    '%EditorLinkFormDuplicated%',
+                ],
+                'gravityforms_forms',
+                'duplicated',
+            ],
 
-			array(
-				5709,
-				WSAL_LOW,
-				__( 'A form was submitted', 'wsal-gravity-forms' ),
-				__( 'Form name: %form_name% %LineBreak% Form ID: %form_id% %LineBreak% Submission email: %email% %LineBreak% %EntryLink%', 'wsal-gravity-forms' ),
-				'gravityforms_forms',
-				'duplicated',
-			),
+            [
+                5715,
+                WSAL_MEDIUM,
+                __( 'A field was created, modified or deleted', 'wsal-gravity-forms' ),
+                __( 'A field was created, modified or deleted', 'wsal-gravity-forms' ),
+                [
+                    __( 'Field name', 'wsal-gravity-forms' ) => '%field_name%',
+                    __( 'Field type', 'wsal-gravity-forms' ) => '%field_type%',
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EditorLinkForm%',
+                ],
+                'gravityforms_fields',
+                'created',
+            ],
 
-			/*
-			 * Form confirmations.
-			 */
-			array(
-				5705,
-				WSAL_MEDIUM,
-				__( 'A confirmation was created, modified or deleted', 'wsal-gravity-forms' ),
-				__( 'Confirmation name: %confirmation_name% %LineBreak% Confirmation type: %confirmation_type% %LineBreak% Confirmation message: %confirmation_message% %LineBreak% Form name: %form_name% %LineBreak% Form ID: %form_id% %LineBreak% %EditorLinkForm%', 'wsal-gravity-forms' ),
-				'gravityforms_confirmations',
-				'created',
-			),
+            [
+                5709,
+                WSAL_LOW,
+                __( 'A form was submitted', 'wsal-gravity-forms' ),
+                __( 'A form was submitted', 'wsal-gravity-forms' ),
+                [
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                    __( 'Submission email', 'wsal-gravity-forms' ) => '%email%',
+                ],
+                [
+                    '%EntryLink%',
+                ],
+                'gravityforms_forms',
+                'duplicated',
+            ],
 
-			array(
-				5708,
-				WSAL_LOW,
-				__( 'A confirmation was activated or deactivated', 'wsal-gravity-forms' ),
-				__( 'The confirmation %confirmation_name% in the form %form_name% %LineBreak% Form ID: %form_id% %LineBreak% %EditorLinkForm%', 'wsal-gravity-forms' ),
-				'gravityforms_confirmations',
-				'created',
-			),
+            /*
+             * Form confirmations.
+             */
+            [
+                5705,
+                WSAL_MEDIUM,
+                __( 'A confirmation was created, modified or deleted', 'wsal-gravity-forms' ),
+                __( 'A confirmation was created, modified or deleted', 'wsal-gravity-forms' ),
+                [
+                    __( 'Confirmation name', 'wsal-gravity-forms' ) => '%confirmation_name%',
+                    __( 'Confirmation type', 'wsal-gravity-forms' ) => '%confirmation_type%',
+                    __( 'Confirmation message', 'wsal-gravity-forms' ) => '%confirmation_message%',
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EditorLinkForm%',
+                ],
+                'gravityforms_confirmations',
+                'created',
+            ],
 
-			/*
-			 * Form notifications.
-			 */
-			array(
-				5706,
-				WSAL_MEDIUM,
-				__( 'A notification was created, modified or deleted', 'wsal-gravity-forms' ),
-				__( 'Notification name: %notification_name% %LineBreak% Form name: %form_name% %LineBreak% Form ID: %form_id% %LineBreak% %EditorLinkForm%', 'wsal-gravity-forms' ),
-				'gravityforms_notifications',
-				'created',
-			),
+            [
+                5708,
+                WSAL_LOW,
+                __( 'A confirmation was activated or deactivated', 'wsal-gravity-forms' ),
+                __( 'The confirmation %1$confirmation_name% in the form %2$form_name%', 'wsal-gravity-forms' ),
+                [
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EditorLinkForm%',
+                ],
+                'gravityforms_confirmations',
+                'created',
+            ],
 
-			array(
-				5707,
-				WSAL_LOW,
-				__( 'A notification was activated or deactivated', 'wsal-gravity-forms' ),
-				__( 'Notification name: %notification_name% %LineBreak% Form name: %form_name% %LineBreak% Form ID: %form_id% %LineBreak% %EditorLinkForm%', 'wsal-gravity-forms' ),
-				'gravityforms_notifications',
-				'activated',
-			),
+            /*
+             * Form notifications.
+             */
+            [
+                5706,
+                WSAL_MEDIUM,
+                __( 'A notification was created, modified or deleted', 'wsal-gravity-forms' ),
+                [
+                    __( 'Notification name', 'wsal-gravity-forms' ) => '%notification_name%',
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EditorLinkForm%',
+                ],
+                'gravityforms_notifications',
+                'created',
+            ],
 
-			/*
-			 * Form entries.
-			 */
-			array(
-				5710,
-				WSAL_LOW,
-				__( 'An entry was starred or unstarred', 'wsal-gravity-forms' ),
-				__( 'Entry title: %entry_title% %LineBreak% Form name: %form_name% %LineBreak% Form ID: %form_id% %LineBreak% %EntryLink%', 'wsal-gravity-forms' ),
-				'gravityforms_entries',
-				'starred',
-			),
+            [
+                5707,
+                WSAL_LOW,
+                __( 'A notification was activated or deactivated', 'wsal-gravity-forms' ),
+                __( 'A notification was activated or deactivated', 'wsal-gravity-forms' ),
+                [
+                    __( 'Notification name', 'wsal-gravity-forms' ) => '%notification_name%',
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EditorLinkForm%',
+                ],
+                'gravityforms_notifications',
+                'activated',
+            ],
 
-			array(
-				5711,
-				WSAL_LOW,
-				__( 'An entry was marked as read or unread', 'wsal-gravity-forms' ),
-				__( 'Entry title: %entry_title% %LineBreak% Form name: %form_name% %LineBreak% Form ID: %form_id% %LineBreak% %EntryLink%', 'wsal-gravity-forms' ),
-				'gravityforms_entries',
-				'read',
-			),
+            /*
+             * Form entries.
+             */
+            [
+                5710,
+                WSAL_LOW,
+                __( 'An entry was starred or unstarred', 'wsal-gravity-forms' ),
+                __( 'An entry was starred or unstarred', 'wsal-gravity-forms' ),
+                [
+                    __( 'Entry title', 'wsal-gravity-forms' ) => '%entry_title%',
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EntryLink%',
+                ],
+                'gravityforms_entries',
+                'starred',
+            ],
 
-			array(
-				5712,
-				WSAL_MEDIUM,
-				__( 'An entry was moved to trash', 'wsal-gravity-forms' ),
-				__( 'An entry was %event_desc% %LineBreak% Form name: %form_name% %LineBreak% Form ID: %form_id% %LineBreak% %EntryLink%', 'wsal-gravity-forms' ),
-				'gravityforms_entries',
-				'read',
-			),
+            [
+                5711,
+                WSAL_LOW,
+                __( 'An entry was marked as read or unread', 'wsal-gravity-forms' ),
+                __( 'An entry was marked as read or unread', 'wsal-gravity-forms' ),
+                [
+                    __( 'Entry title', 'wsal-gravity-forms' ) => '%entry_title%',
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EntryLink%',
+                ],
+                'gravityforms_entries',
+                'read',
+            ],
 
-			array(
-				5713,
-				WSAL_MEDIUM,
-				__( 'An entry was permanently deleted', 'wsal-gravity-forms' ),
-				__( 'Permanently deleted the entry %entry_title% %LineBreak% Form name: %form_name% %LineBreak% Form ID: %form_id%', 'wsal-gravity-forms' ),
-				'gravityforms_entries',
-				'read',
-			),
+            [
+                5712,
+                WSAL_MEDIUM,
+                __( 'An entry was moved to trash', 'wsal-gravity-forms' ),
+                __( 'An entry was moved to trash', 'wsal-gravity-forms' ),
+                [
+                    __( 'An entry was', 'wsal-gravity-forms' ) => '%event_desc%',
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID ', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EntryLink%',
+                ],
+                'gravityforms_entries',
+                'read',
+            ],
 
-			array(
-				5714,
-				WSAL_MEDIUM,
-				__( 'An entry note was created or deleted', 'wsal-gravity-forms' ),
-				__( 'The entry note %entry_note% %LineBreak% Entry title: %entry_title% %LineBreak% Form name: %form_name% %LineBreak% Form ID: %form_id% %LineBreak% %EntryLink%', 'wsal-gravity-forms' ),
-				'gravityforms_entries',
-				'read',
-			),
+            [
+                5713,
+                WSAL_MEDIUM,
+                __( 'An entry was permanently deleted', 'wsal-gravity-forms' ),
+                __( 'An entry was permanently deleted', 'wsal-gravity-forms' ),
+                [
+                    __( 'Permanently deleted the entry', 'wsal-gravity-forms' ) => '%entry_title%',
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [],
+                'gravityforms_entries',
+                'read',
+            ],
 
-			/*
-			 * Settings.
-			 */
-			array(
-				5716,
-				WSAL_HIGH,
-				__( 'A plugin setting was changed.', 'wsal-gravity-forms' ),
-				__( 'The plugin setting %setting_name% %LineBreak% Previous value %old_value% %LineBreak% New value: %new_value% %LineBreak%', 'wsal-gravity-forms' ),
-				'gravityforms_settings',
-				'modified',
-			),
+            [
+                5714,
+                WSAL_MEDIUM,
+                __( 'An entry note was created or deleted', 'wsal-gravity-forms' ),
+                __( 'An entry note was created or deleted', 'wsal-gravity-forms' ),
+                [
+                    __( 'The entry note', 'wsal-gravity-forms' ) => '%entry_note%',
+                    __( 'Entry title', 'wsal-gravity-forms' ) => '%entry_title%',
+                    __( 'Form name', 'wsal-gravity-forms' ) => '%form_name%',
+                    __( 'Form ID', 'wsal-gravity-forms' ) => '%form_id%',
+                ],
+                [
+                    '%EntryLink%',
+                ],
+                'gravityforms_entries',
+                'read',
+            ],
 
-		),
-	),
-);
+            /*
+             * Settings.
+             */
+            [
+                5716,
+                WSAL_HIGH,
+                __( 'A plugin setting was changed.', 'wsal-gravity-forms' ),
+                __( 'A plugin setting was changed.', 'wsal-gravity-forms' ),
+                [
+                    __( 'The plugin setting', 'wsal-gravity-forms' ) => '%setting_name%',
+                    __( 'Previous value', 'wsal-gravity-forms' ) => '%old_value%',
+                    __( 'New value', 'wsal-gravity-forms' ) => '%new_value%',
+                ],
+                [],
+                'gravityforms_settings',
+                'modified',
+            ],
+        ],
+    ],
+];
